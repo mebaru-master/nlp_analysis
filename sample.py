50135b8a5f36a91b078359342972774312cae5ae
mebaru-master <154340605+mebaru-master@users.noreply.github.com>
2024-11-27 01:01:14 +0000

    update script

--- sample.py
+++ sample.py
@@ -1,19 +1,21 @@
 import os
 import sys
 from dotenv import load_dotenv
+import pandas as pd
 
 # .envを読み込む
 load_dotenv()
 print(os.getenv('email'))
 
+# パスを追加する
+sys.path.append('../data')
+# ファイルパスを作成する
+file_path = os.path.join(os.path.dirname(__file__), 'data/Raw_Data.csv')
 # iCloud driveにあるcsvファイルを読み込む
-import pandas as pd
 try:
-    df = pd.read_csv('data/Raw_Data.csv')
+    df = pd.read_csv(file_path)
 except FileNotFoundError:
     print('File not found')
     sys.exit(1)
 
 print(df)
-
-
